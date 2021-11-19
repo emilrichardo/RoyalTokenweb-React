e2941ce10fa93d90aa536ae7d568deb151ac2c6d
emilrichardo <emilrichardo@gmail.com>
2021-11-19 18:14:53 -0300

    configure trasnlation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {I18nextProvider} from "react-i18next";
+import i18next from "i18next";
 
+import global_es from "./traslations/es/global.json";
+import global_en from "./traslations/en/global.json";
+
+i18next.init({
+  interpolation: {escapeValue:false},
+  lng: "es",
+  resources:{
+    es:{
+      global: global_es,
+    },
+    en:{
+      global: global_en,
+    },
+  },
+});
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
