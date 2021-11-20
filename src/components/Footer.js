bc243e1c51d4a1346b90e3f5a5fc19c3fa723f13
emilrichardo <emilrichardo@gmail.com>
2021-11-20 15:46:49 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 const Footer = () => {
-    return ( <>
-    footer
-
-    <Helmet>
-
-
-    </Helmet>
+    return (
+    <>
+    <footer>
+			<section>
+				<div class="footer">
+					<p>NFT Royal Token © 2021. All rights reserved</p>
+					<p>Crafted by <a href="https://disruptivestudio.com/" target="_BLANK" class="disruptive"></a></p>
+				</div>
+			</section>
+		</footer>
     </> );
 }
 
